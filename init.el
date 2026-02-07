52388872d668f77a22e74bca16f0a549d5dcf3b6
ja0nz <git@ja.nz>
2026-02-07 16:52:02 +0100

    feat: lsp-mode -> eglot

--- init.el
+++ init.el
@@ -262,14 +262,6 @@
       (side . bottom)
       (slot . 0))
 
-     ;; Example configuration for the LSP help buffer,
-     ;; keeps it always on bottom using 25% of the available space:
-     ("\\*\\(lsp-help\\)\\*"
-      (display-buffer-in-side-window)
-      (window-height . 0.25)
-      (side . bottom)
-      (slot . 0))
-
      ;; Configuration for displaying various diagnostic buffers on
      ;; bottom 25%:
      ("\\*\\(Flymake diagnostics\\|xref\\|ivy\\|Swiper\\|Completions\\)"
@@ -629,93 +621,68 @@
   :defer t
   :after (:all corfu))
 
+;;;;;; EGLOT Family
+;;;;;;;; Breadcrumb
+(use-package breadcrumb
+  :hook
+  (eglot-connect . breadcrumb-mode))
 
-;;; LSP
-;; Emacs comes with an integrated LSP client called `eglot', which offers basic LSP functionality.
-;; However, `eglot' has limitations, such as not supporting multiple language servers
-;; simultaneously within the same buffer (e.g., handling both TypeScript, Tailwind and ESLint
-;; LSPs together in a React project). For this reason, the more mature and capable
-;; `lsp-mode' is included as a third-party package, providing advanced IDE-like features
-;; and better support for multiple language servers and configurations.
-;;
-;; NOTE: To install or reinstall an LSP server, use `M-x install-server RET`.
-;;       As with other editors, LSP configurations can become complex. You may need to
-;;       install or reinstall the server for your project due to version management quirks
-;;       (e.g., asdf or nvm) or other issues.
-;;       Fortunately, `lsp-mode` has a great resource site:
-;;       https://emacs-lsp.github.io/lsp-mode/
-(use-package lsp-mode
-  :hook (;; Replace XXX-mode with concrete major mode (e.g. python-mode)
-         (lsp-mode . lsp-enable-which-key-integration)  ;; Integrate with Which Key
-         ((js-mode                                      ;; Enable LSP for JavaScript
-           tsx-ts-mode                                  ;; Enable LSP for TSX
-           typescript-ts-base-mode                      ;; Enable LSP for TypeScript
-           css-mode                                     ;; Enable LSP for CSS
-           go-ts-mode                                   ;; Enable LSP for Go
-           js-ts-mode                                   ;; Enable LSP for JavaScript (TS mode)
-           prisma-mode                                  ;; Enable LSP for Prisma
-           python-base-mode                             ;; Enable LSP for Python
-           ruby-base-mode                               ;; Enable LSP for Ruby
-           rust-ts-mode                                 ;; Enable LSP for Rust
-           nix-mode                                     ;; Enable LSP for Nix
-           web-mode) . lsp-deferred))                   ;; Enable LSP for Web (HTML)
-  :commands lsp
+;;;;;;;; Eglot
+(use-package eglot
+  :ensure nil
   :custom
-  (lsp-keymap-prefix "C-c l")                           ;; Set the prefix for LSP commands.
-  (lsp-inlay-hint-enable nil)                           ;; Usage of inlay hints.
-  (lsp-completion-provider :none)                       ;; Disable the default completion provider.
-  (lsp-session-file (locate-user-emacs-file ".lsp-session")) ;; Specify session file location.
-  (lsp-log-io nil)                                      ;; Disable IO logging for speed.
-  (lsp-idle-delay 0.5)                                  ;; Set the delay for LSP to 0 (debouncing).
-  (lsp-keep-workspace-alive nil)                        ;; Disable keeping the workspace alive.
-  ;; Core settings
-  (lsp-enable-xref t)                                   ;; Enable cross-references.
-  (lsp-auto-configure t)                                ;; Automatically configure LSP.
-  (lsp-enable-links nil)                                ;; Disable links.
-  (lsp-eldoc-enable-hover t)                            ;; Enable ElDoc hover.
-  (lsp-enable-file-watchers nil)                        ;; Disable file watchers.
-  (lsp-enable-folding nil)                              ;; Disable folding.
-  (lsp-enable-imenu t)                                  ;; Enable Imenu support.
-  (lsp-enable-indentation nil)                          ;; Disable indentation.
-  (lsp-enable-on-type-formatting nil)                   ;; Disable on-type formatting.
-  (lsp-enable-suggest-server-download t)                ;; Enable server download suggestion.
-  (lsp-enable-symbol-highlighting t)                    ;; Enable symbol highlighting.
-  (lsp-enable-text-document-color t)                    ;; Enable text document color.
-  ;; Modeline settings
-  (lsp-modeline-code-actions-enable nil)                ;; Keep modeline clean.
-  (lsp-modeline-diagnostics-enable nil)                 ;; Use `flymake' instead.
-  (lsp-modeline-workspace-status-enable t)              ;; Display "LSP" in the modeline when enabled.
-  (lsp-signature-doc-lines 1)                           ;; Limit echo area to one line.
-  (lsp-eldoc-render-all t)                              ;; Render all ElDoc messages.
-  ;; Completion settings
-  (lsp-completion-enable t)                             ;; Enable completion.
-  (lsp-completion-enable-additional-text-edit t)        ;; Enable additional text edits for completions.
-  (lsp-enable-snippet nil)                              ;; Disable snippets
-  (lsp-completion-show-kind t)                          ;; Show kind in completions.
-  ;; Lens settings
-  (lsp-lens-enable t)                                   ;; Enable lens support.
-  ;; Headerline settings
-  (lsp-headerline-breadcrumb-enable-symbol-numbers t)   ;; Enable symbol numbers in the headerline.
-  (lsp-headerline-arrow "▶")                            ;; Set arrow for headerline.
-  (lsp-headerline-breadcrumb-enable-diagnostics nil)    ;; Disable diagnostics in headerline.
-  (lsp-headerline-breadcrumb-icons-enable nil)          ;; Disable icons in breadcrumb.
-  ;; Semantic settings
-  (lsp-semantic-tokens-enable nil)                     ;; Disable semantic tokens.
-  ;; Language settings
-  (lsp-nix-nil-formatter ["nixfmt"]))
-
-;;; LSP Additional Servers
-;; You can extend `lsp-mode' by integrating additional language servers for specific
-;; technologies. For example, `lsp-tailwindcss' provides support for Tailwind CSS
-;; classes within your HTML files. By using various LSP packages, you can connect
-;; multiple LSP servers simultaneously, enhancing your coding experience across
-;; different languages and frameworks.
-(use-package lsp-tailwindcss
-  :after lsp-mode
+  (eglot-autoshutdown t)
+  ;; PROD mode, truncate events buffer
+  (eglot-events-buffer-config '(:size 0 :format full))
+  (eglot-prefer-plaintext nil)
+  (jsonrpc-event-hook nil)
+  (eglot-code-action-indications nil) ;; EMACS-31 -- annoying as hell
+  :hook ((prog-mode . (lambda ()
+                        (unless (derived-mode-p 'emacs-lisp-mode)
+                          (eglot-ensure)))))
   :config
-  (add-to-list 'lsp-language-id-configuration '(".*\\.erb$" . "html")) ;; Associate ERB files with HTML.
+  (add-to-list 'eglot-server-programs
+               `(python-mode
+                 . ,(eglot-alternatives '("pylsp"
+                                          ("pyright-langserver" "--stdio")
+                                          "jedi-language-server"
+                                          "pylsp"))))
+
+  ;; Aditional:
+  ;; gd -> xref-find-definitions
+  ;; gr -> xref-find-references
+  ;; xref-go-back and xref-find-apropos
+  :bind (:map
+         eglot-mode-map
+         ("<leader> l g t" . eglot-find-typeDefinition)
+         ("<leader> l g d" . eglot-find-declaration)
+         ("<leader> l g i" . eglot-find-implementation)
+         ("<leader> l a" . eglot-code-actions)
+         ("<leader> l o" . eglot-code-action-organize-imports)
+         ("<leader> l r" . eglot-rename)
+         ("<leader> l i" . eglot-inlay-hints-mode)
+         ("<leader> l f" . eglot-format))
+
   :init
-  (setq lsp-tailwindcss-add-on-mode t))
+  ;; Special commands flags passed to language servers
+  ;; Better: put in .dir-locals.el per project
+  ;; (setq-default eglot-workspace-configuration (quote
+  ;;                                              (:gopls (:hints (:parameterNames t)))))
+
+  ;; Mute Eglot chatter
+  (fset #'jsonrpc--log-event #'ignore))
+
+  ;; (with-eval-after-load 'eglot
+  ;;   (add-to-list
+  ;;    'eglot-server-programs
+  ;;    '((tsx-ts-mode typescript-ts-mode js-mode js-jsx-mode js-ts-mode)
+  ;;      . ("rass"
+  ;;         "--"
+  ;;         "typescript-language-server" "--stdio"
+  ;;         "--"
+  ;;         "eslint-lsp" "--stdio"
+  ;;         "--"
+  ;;         "tailwindcss-language-server" "--stdio"))))
 
 ;;; ELDOC-BOX
 ;; eldoc-box enhances the default Eldoc experience by displaying documentation in a popup box,
@@ -994,31 +961,7 @@
       (shell-command (concat "prettier --write " (shell-quote-argument (buffer-file-name))))
       (revert-buffer t t t)))
 
-  ;; LSP commands keybindings
-  (evil-define-key 'normal lsp-mode-map
-    ;; (kbd "gd") 'lsp-find-definition                ;; evil-collection already provides gd
-    (kbd "gr") 'lsp-find-references                   ;; Finds LSP references
-    (kbd "<leader> c a") 'lsp-execute-code-action     ;; Execute code actions
-    (kbd "<leader> r n") 'lsp-rename                  ;; Rename symbol
-    (kbd "gI") 'lsp-find-implementation               ;; Find implementation
-    (kbd "<leader> l f") 'lsp-format-buffer)          ;; Format buffer via lsp
-
-
-  (defun ek/lsp-describe-and-jump ()
-    "Show hover documentation and jump to *lsp-help* buffer."
-    (interactive)
-    (lsp-describe-thing-at-point)
-    (let ((help-buffer "*lsp-help*"))
-      (when (get-buffer help-buffer)
-        (switch-to-buffer-other-window help-buffer))))
-
-  ;; Emacs 31 finaly brings us support for 'floating windows' (a.k.a. "child frames")
-  ;; to terminal Emacs. If you're still using 30, docs will be shown in a buffer at the
-  ;; inferior part of your frame.
-  (evil-define-key 'normal 'global (kbd "K")
-    (if (>= emacs-major-version 31)
-        #'eldoc-box-help-at-point
-      #'ek/lsp-describe-and-jump))
+  (evil-define-key 'normal 'global (kbd "K") #'eldoc-box-help-at-point)
 
   ;; Commenting functionality for single and multiple lines
   (evil-define-key 'normal 'global (kbd "gcc")
